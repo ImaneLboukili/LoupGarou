22e34dc3c5319082acdc83cd0c5563bd44f086ff
Oceane Csn <oceane.cassan@insa-lyon.fr>
2017-10-03 11:29:06 +0200

    pimp interface

--- Interface.py
+++ Interface.py
@@ -6,7 +6,7 @@ class Partie:
 	
 	def __init__(self, root):
 		self.user = ""
-		
+		self.chat = ""
 		self.root = root
 		self.s = Label(text ="Bienvenue", font=(('Times'),20))
 		self.s.grid(row=2,column=0)
@@ -24,8 +24,8 @@ class Partie:
 		self.b.grid(row=5, column=0)
 
 		
-		TI = Button(text="Play", width =50, command=lambda: self.Vote(self.user))
-		TI.grid(row=6, column=0)
+		play = Button(text="Play", width =50, command=lambda: self.Vote(self.user))
+		play.grid(row=6, column=0)
 
 		root.mainloop()
 		
@@ -33,18 +33,22 @@ class Partie:
 		self.user = self.e.get()
 		print self.user
 		
+	def getchat(self):
+		self.chat += "\n" +self.user+" : " + self.field.get()
+		self.chatLabel.config(text=self.chat)
+		
 	def Vote(self, user):
 		top=Toplevel()
 		top.title("Let's vote...")
 		notiLabel = Label(top, text ="Qui souhaitez vous tuer, "+self.user+" ?", font=('Times', 20))
 		notiLabel.grid(row=0,column=0, sticky=W)
 		
-		notiLabel = Label(top, text ="Chat", font=('Times', 16), height = 20)
-		notiLabel.grid(row=1,column=0, sticky=W)
+		self.chatLabel = Label(top, text ="Chat", font=('Times', 14), height = 20, width = 40, background="white")
+		self.chatLabel.grid(row=1,column=0, sticky=W)
 		
-		field = Entry(top)
-		field.focus_set()
-		field.grid(row=2, column=0)
+		self.field = Entry(top)
+		self.field.focus_set()
+		self.field.grid(row=2, column=0)
 
 		listbox = Listbox(top)
 		listbox.grid(row=1,column=1, sticky=W)
@@ -52,6 +56,10 @@ class Partie:
 		listbox.insert(END, "list of users")
 		b = Button(top, text="You die")
 		b.grid(row=2, column=1)
+		
+		ok = Button(top, text="Ok", width =50, command=self.getchat)
+		ok.grid(row=7, column=0)
+
 
 		for item in ["Imane", "Louise", "Pierre", "Oceane"]:
 			listbox.insert(END, item)
